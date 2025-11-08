330247032a96e8ad24877cf2be37c9e371bc007f
TianYi <2739022972@qq.com>
2025-11-08 14:43:01 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
